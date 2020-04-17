724b2751396075187b6f849feb5a3a333290e8bb
Vikram <snipperbytes@gmail.com>
2020-04-17 22:43:18 +0530

    updating main.go #1

--- cmd/covidbot/main.go
+++ cmd/covidbot/main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
+  "fmt"
   "log"
+  "telegram-covid19-bot/keyboard"
   "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-var numericKeyboard = tgbotapi.NewReplyKeyboard(
+var continentKeyboard = tgbotapi.NewReplyKeyboard(
   tgbotapi.NewKeyboardButtonRow(
-    tgbotapi.NewKeyboardButton("Asia"),
-    tgbotapi.NewKeyboardButton("Africa"),
+    tgbotapi.NewKeyboardButton("/asia"),
+    tgbotapi.NewKeyboardButton("/africa"),
   ),
   tgbotapi.NewKeyboardButtonRow(
-    tgbotapi.NewKeyboardButton("America"),
-    tgbotapi.NewKeyboardButton("Europe"),
+    tgbotapi.NewKeyboardButton("/america"),
+    tgbotapi.NewKeyboardButton("/europe"),
   ),
 )
 
 func main() {
-  bot, err := tgbotapi.NewBotAPI("XXXXXX-xxxxx-XXXXXXXX")
+  bot, err := tgbotapi.NewBotAPI("1104178846:AAEG1Gl29ME2ONS-j9FPmMW10jzsaRa86to")
   if err != nil {
     log.Panic(err)
   }
 
   bot.Debug = true
-
   log.Printf("Authorized on account %s", bot.Self.UserName)
 
   u := tgbotapi.NewUpdate(0)
@@ -36,12 +37,22 @@ func main() {
       continue
     }
     msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+    fmt.Println(msg)
+    log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
     switch update.Message.Text {
-    case "open":
-      msg.ReplyMarkup = numericKeyboard
-    case "close":
+    case "/globalStatus":
       msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+    case "/continents":
+      msg.ReplyMarkup = continentKeyboard
+    case "/asia":
+      msg.ReplyToMessageID = keyboard.AsiaKeyboard
+    case "/africa":
+      msg.ReplyMarkup = keyboard.AfricaKeyboard
+    case "/america":
+      msg.ReplyMarkup = keyboard.AmericaKeyboard
+    case "/europe":
+      msg.ReplyMarkup = keyboard.EuropeKeyboard
     }
 
     bot.Send(msg)
