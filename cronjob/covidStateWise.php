5582b364928f7547f97ac1132557f2389e293bfa
Vikram <snipperbytes@gmail.com>
2020-04-19 00:01:46 +0530

    adding statewise api

---
+++ cronjob/covidStateWise.php
@@ -0,0 +1,20 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://ameerthehacker.github.io/corona-india-status/covid19-indian-states.json",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+var_dump(json_decode($response,true));
+
+?>
