b8f5358b218eea68f34791507b6d848307a68d83
Vikram <snipperbytes@gmail.com>
2020-04-18 23:52:56 +0530

    test file to be implemented for redis hgetall

---
+++ testing.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "github.com/go-redis/redis"
+  "fmt"
+)
+
+func main() {
+  client := redis.NewClient(&redis.Options{
+    Addr:     "localhost:6379",
+    Password: "", // no password set
+    DB:       0,  // use default DB
+  })
+
+  datasize := make(map[string]string)
+  datasize, _ = client.HGetAll("india").Result()
+  fmt.Println(datasize["NewConfirmed"])
+  pong, err := client.Ping().Result()
+  fmt.Println(pong, err)
+  // Output: PONG <nil>
+}
+
+
+
