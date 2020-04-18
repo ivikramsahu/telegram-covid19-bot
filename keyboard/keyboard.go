2e74711f31aaeec40bd49773b8e5a8d01bd165fc
Vikram <snipperbytes@gmail.com>
2020-04-18 22:04:07 +0530

    updating keyboards country-wise

--- keyboard/keyboard.go
+++ keyboard/keyboard.go
@@ -1,89 +1,231 @@
 package keyboard
 
-import(
-  "github.com/go-telegram-bot-api/telegram-bot-api"
-)
+//CoronaVirus Update
+var CoronaVirus = `
+**About COVID-19**
+Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus .
+
+Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment. 
+Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.
+
+**How you can prevent?**
+
+The best way to prevent and slow down transmission is be well informed about the COVID-19 virus, the disease it causes and how it spreads.
+Protect yourself and others from infection by washing your hands or using an alcohol based rub frequently and not touching your face.
+
+The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes,
+so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).
+`
+
+
 
 //AsiaKeyboard countries keyboard
 var AsiaKeyboard =`
------------------------------------------
-            Asian countries               
------------------------------------------      
-1. /india           25./cambodia
-2. /china           26./jordan
-3. /indonesia       27./azerbaijan
-4. /pakistan        28./uae
-5. /bangalesh       29./tajikistan
-6. /japan           30./israel
-7. /philippines     31./laos
-8. /vietnam         32./lebanon
-9. /turkey          33./kyrgyzstan
-10./iran            34./turkmenistan
-11./thailand        35./singapore
-12./myanmar         36./oman
-13./south_korea     37./stateofpalestine
-14./iraq            38./kuwait
-15./afghanistan     39./georgia
-16./saudi_arabia    40./mongolia
-17./uzbekistan      41./armenia
-18./malaysia        42./qatar
-19./yemen           43./bahrain
-20./nepal           44./timorleste
-21./north_korea     45./cyprus
-22./srilanka        46./bhutan
-23./kazakhstan      47./maldives
-24./syria           48./brunei
+---------------------------
+     Asian countries
+---------------------------   
+1.  /india          
+2.  /china           
+3.  /indonesia       
+4.  /pakistan        
+5.  /bangalesh       
+6.  /japan           
+7.  /philippines     
+8.  /vietnam         
+9.  /turkey         
+10. /iran            
+11. /thailand        
+12. /myanmar         
+13. /south_korea     
+14. /iraq            
+15. /afghanistan     
+16. /saudi_arabia    
+17. /uzbekistan      
+18. /malaysia        
+19. /yemen           
+20. /nepal           
+21. /north_korea     
+22. /srilanka        
+23. /kazakhstan      
+24. /syria           
+25. /cambodia
+26. /jordan
+27. /azerbaijan
+28. /uae
+29. /tajikistan
+30. /israel
+31. /laos
+32. /lebanon
+33. /kyrgyzstan
+34. /turkmenistan
+35. /singapore
+36. /oman
+37. /stateofpalestine
+38. /kuwait
+39. /georgia
+40. /mongolia
+41. /armenia
+42. /qatar
+43. /bahrain
+44. /timorleste
+45. /cyprus
+46. /bhutan
+47. /maldives
+48. /brunei
+----------------------------
+  Select your country
+----------------------------
 `
 //AfricaKeyboard continents keyboard
-var AfricaKeyboard = tgbotapi.NewReplyKeyboard(
-  tgbotapi.NewKeyboardButtonRow(
-    tgbotapi.NewKeyboardButton("East Africa"),
-    tgbotapi.NewKeyboardButton("West Africa"),
-  ),
-  tgbotapi.NewKeyboardButtonRow(
-    tgbotapi.NewKeyboardButton("North Africa"),
-    tgbotapi.NewKeyboardButton("South Africa"),
-  ),
-)
+var AfricaKeyboard =`
+1.  /nigeria
+2.  /ethiopia
+3.  /egypt
+4.  /dr_congo
+5.  /tanzania
+6.  /south_africa
+7.  /kenya
+8.  /uganda
+9.  /algeria
+10. /sudan
+11. /morocco
+12. /angola
+13. /mozambique
+14. /ghana
+15. /madagascar
+16. /cameroon
+17. /côte_d_ivoire
+18. /niger
+19. /burkina_faso
+20. /mali
+21. /malawi
+22. /zambia
+23. /senegal
+24. /chad
+25. /somalia
+26. /zimbabwe
+27. /guinea
+28. /rwanda
+29. /benin
+28. /burundi
+29. /tunisia
+30. /south_sudan
+31. /togo
+32. /sierra_leone
+33. /libya
+34. /congo
+35. /liberia
+36. /central_african_republic
+37. /mauritania
+38. /eritrea
+39. /namibia
+40. /gambia
+41. /botswana
+42. /gabon
+43. /lesotho
+44. /guinea_bissau
+45. /equatorial_guinea
+46. /mauritius
+47. /eswatini
+48. /djibouti
+49. /comoros
+50. /cabo_verde
+51. /sao_tome_and_principe
+52. /seychelles
+`
 
 //EuropeKeyboard countries keyboard
 var EuropeKeyboard = `
------------------------------------------
-European countries               
------------------------------------------      
-1. /russia          23./finland
-2. /germany         24./slovakia
-3. /UK              25./norway
-4. /france          26./ireland
-5. /italy           27./croatia
-6. /spain           28./moldova
-7. /ukraine         29./bosnia_and_herzegovina
-8. /poland          30./albania
-9. /romania         31./lithuania
-10./netherlands     32./north_macedonia
-11./belgium         33./slovenia
-12./czech_republic  34./latvia
-13./greece          35./estonia
-14./portugal        36./montenegro
-15./sweden          37./luxembourg
-16./hungary         38./malta
-17./belarus         39./andorra
-18./austria         40./iceland
-19./serbia          41./monaco
-20./switzerland     42./liechtenstein
-21./bulgaria        43./san_marino
-22./denmark         44./holy_see
------------------------------------------
+---------------------------------
+      European countries               
+---------------------------------      
+1.  /russia        
+2.  /germany       
+3.  /uk            
+4.  /france        
+5.  /italy         
+6.  /spain         
+7.  /ukraine       
+8.  /poland        
+9.  /romania       
+10. /netherlands   
+11. /belgium       
+12. /czech_republic
+13. /greece        
+14. /portugal      
+15. /sweden        
+16. /hungary       
+17. /belarus       
+18. /austria       
+19. /serbia        
+20. /switzerland   
+21. /bulgaria      
+22. /denmark       
+23. /finland
+24. /slovakia
+25. /norway
+26. /ireland
+27. /croatia
+28. /moldova
+29. /bosnia_and_herzegovina
+30. /albania
+31. /lithuania
+32. /north_macedonia
+33. /slovenia
+34. /latvia
+35. /estonia
+36. /montenegro
+37. /luxembourg
+38. /malta
+39. /andorra
+40. /iceland
+41. /monaco
+42. /liechtenstein
+43. /san_marino
+44. /holy_see
+----------------------------
+  Select your country
+----------------------------
 `
 
 //AmericaKeyboard countries keyboard
-var AmericaKeyboard = tgbotapi.NewReplyKeyboard(
-  tgbotapi.NewKeyboardButtonRow(
-    tgbotapi.NewKeyboardButton("Asia"),
-    tgbotapi.NewKeyboardButton("Africa"),
-  ),
-  tgbotapi.NewKeyboardButtonRow(
-    tgbotapi.NewKeyboardButton("America"),
-    tgbotapi.NewKeyboardButton("Europe"),
-  ),
-)
+var AmericaKeyboard =`
+----------------------------
+   American Countries
+----------------------------
+1.  /canada
+2.  /chile
+3.  /colombia
+4.  /costa_rica
+5.  /cuba
+6.  /ecuador
+7.  /el_salvador
+8.  /antigua_and_barbuda
+9.  /grenada
+10. /argentina
+11. /guatemala
+12. /guyana
+13. /honduras
+14. /jamaica
+15. /mexico
+16. /barbados
+17. /nicaragua
+18. /panama
+19. /paraguay
+20. /peru
+21. /saint_kitts_and_nevis
+22. /saint_lucia
+23. /saint_vincent_and_the_grenadines
+24. /bolivia
+25. /suriname
+26. /brazil
+27. /trinidad_and_tobago
+28. /belize
+29. /us
+30. /uruguay
+31. /venezuela
+----------------------------
+  Select your country
+----------------------------
+`
+
